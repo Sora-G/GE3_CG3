aec4ea6ff90dcf37271422b3827e7fcf21858cb4
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-28 15:21:08 +0900

    02-01完成

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <sstream>
 #include "Input.h"
+#include "wrl.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 

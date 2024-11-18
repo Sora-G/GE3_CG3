7f189e6b32b90dc51da292f04d947a316c5dc4f0
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-11-18 14:24:49 +0900

    ウィンドウが真っ白

--- main.cpp
+++ main.cpp
@@ -1267,7 +1267,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	//入力開放
 	delete input;
 
-	winApp = nullptr;
+	//winApp = nullptr;
 	//WindowsAPIの終了処理
 	winApp->Finalize();
 	//WindowsAPI解放

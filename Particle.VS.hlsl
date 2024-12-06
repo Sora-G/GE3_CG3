99524fd2f4bb463a75ff1bf08d55338bb969f40c
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-12-06 10:21:08 +0900

    TransdromMatrix構造体のWorld変数の位置を変更

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -2,8 +2,8 @@
 
 struct TransformationMatrix
 {
-    float32_t4x4 World;
     float32_t4x4 WVP;
+    float32_t4x4 World;
 };
 
 StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);

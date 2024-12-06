c148ff37d9a49092e6dd5cb64680c4d96318544e
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-12-06 10:35:53 +0900

    float32_~~をfloatの形に変更

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -2,16 +2,16 @@
 
 struct TransformationMatrix
 {
-    float32_t4x4 WVP;
-    float32_t4x4 World;
+    float4x4 WVP;
+    float4x4 World;
 };
 
 StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 
 struct VertexShaderInput
 {
-    float32_t4 position : POSITION0;
-    float32_t2 texcoord : TEXCOORD0;
+    float4 position : POSITION0;
+    float2 texcoord : TEXCOORD0;
 };
 
 VertexShaderOutput main(VertexShaderInput input,uint32_t instanced : SV_InstanceID)

7946bcd542f39a5b256bce52a55caf9b0aa4a340
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-02-10 16:00:55 +0900

    デストラクタの作りかけ

---
+++ D3DReourceLeakChecker.cpp
@@ -0,0 +1,11 @@
+#include "D3DReourceLeakChecker.h"
+#include <dxgidebug.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+
+D3DReourceLeakChecker::~D3DReourceLeakChecker()
+{
+	//リソースリークチェック
+	Microsoft::WRL::ComPtr
+}
